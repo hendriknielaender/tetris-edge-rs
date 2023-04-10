21cf535a16ae84089757c81ede4d23f080c58e36
Hendrik Nieländer <hendrik@nielaender.de>
2023-04-10 21:37:16 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tetris-edge-rs
+
+### Build
+`cargo build --target wasm32-unknown-unknown`
+
+### Compile to wasm to js
+`wasm-bindgen --target=web ./target/wasm32-unknown-unknown/debug/tetris_edge_rs.wasm --out-dir=pkg`
+
+### Run locally
+`npx serve .`
---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+use wasm_bindgen::prelude::*;
+use web_sys;
+
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
